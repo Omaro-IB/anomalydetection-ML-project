78c75ced0410073d6e243b9754b55ff3342b6bf7
Omar Ibrahim <omaro2003@live.com>
2022-06-17 17:54:56 +0200

    processed using random forest regressor to find y-hat

--- __main__.py
+++ __main__.py
@@ -6,9 +6,9 @@ import pandas as pd
 
 #   ALL WEEKS
 # DATA
-data_frame = data_exploration.initialize_df("train.csv", index="timestamp", drop=["anomaly",
-                                                                                  "building_id"])  # Timestamp index data-frame (Only meter-reading)
+data_frame = data_exploration.initialize_df("train.csv", index="timestamp", drop=["anomaly","building_id"])  # Timestamp index data-frame (Only meter-reading)
 data_frame2 = data_exploration.initialize_df("train.csv", drop=["building_id"], parse_dates=["timestamp"]) #Data frame to be used for training
+data_frame_RFR = data_exploration.create_lag_df(data_exploration.initialize_df("train.csv", drop=["anomaly","building_id"]), "meter_reading", "timestamp")
 # data_frame_indexed = data_exploration.initialize_df("train.csv", drop=["anomaly", "building_id","timestamp"])  # Indexed data-frame (Only meter_reading)
 # data_frame_indexed_anomaly = data_exploration.initialize_df("train.csv", drop=["building_id", "meter_reading","timestamp"])  # Indexed data-frame (meter-reading and anomaly)
 
@@ -16,12 +16,18 @@ data_frame2 = data_exploration.initialize_df("train.csv", drop=["building_id"], 
 
 #   WEEK 2
 #TRAINING
-train_df, test_df = train.split(data_frame2, 0.8, seed=192881) #train-test 80% split
+train_df, test_df = train.split(data_frame2, 0.8, seed=192881) # train-test 80% split
 #graphing.plot_scatter(train_df, "timestamp", "meter_reading") # plot data
 
-std_train_df = train.standardize_data(train_df, "meter_reading") #standardize data
+std_train_df = train.standardize_data(train_df, "meter_reading") # standardize data
 #graphing.plot_scatter(pd.DataFrame(std_train_df), "timestamp", "meter_reading") # plot standardized data
 
+std_RFR = train.standardize_data((train.standardize_data(data_frame_RFR, "lag1")),"lag2") # standardize RFR data
+sup_std_RFR = data_exploration.series_to_supervised(std_RFR) # supervise standardized RFR data
+
+print(sup_std_RFR)
+yhat = train.random_forest_forecast(sup_std_RFR[0:len(sup_std_RFR):500]) #find yhat of supervised standardized RFR data
+print(yhat)
 
 
 #   WEEK 1
--- data_exploration.py
+++ data_exploration.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from datetime import datetime
+from sklearn.metrics import mean_absolute_error
 
 
 def initialize_df(dir: object, index: object = None, drop: object = [], parse_dates: object = []) -> object:  # initializes the data-frame
@@ -25,7 +26,6 @@ def initialize_df(dir: object, index: object = None, drop: object = [], parse_da
         df = df.drop(d, axis=1)
     return df
 
-
 def create_time_series(df, lag=False):
     """
     Creates a non-indexed Pandas Dataframe with either lag values as integers (from first date in days) or just the
@@ -48,3 +48,54 @@ def create_time_series(df, lag=False):
             dates_f.append(datetime.strptime(date[2:], '%y-%m-%d %H:%M:%S'))
             time_column = pd.Series(dates_f)
     return time_column
+
+def create_lag_df(df, column, lag):
+    """
+    Creates a Pandas dataframe with a lag column
+    :param df:Pandas dataframe, with no index column, two columns of the same size
+    :param column:String, The column name to be lagged
+    :param lag:String, The other column name to be replaced with the  column
+    :return:Pandas DataFrame, with the lag column replaced with the column but lagged (shifted up by 1)
+    """
+    df[lag] = df[column]
+    df = df.rename(columns={column: 'lag2', lag: "lag1"})
+    df["lag1"] = df["lag1"].shift(-1)
+    return df[:-1]
+
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    # transform a time series dataset into a supervised learning dataset
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = pd.DataFrame(data)
+    cols = list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+    # put it all together
+    agg = pd.concat(cols, axis=1)
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg.values
+
+# def walk_forward_validation(train, test):
+#     predictions = list()
+#     # seed history with training dataset
+#     history = [x for x in train]
+#     # step over each time-step in the test set
+#     for i in range(len(test)):
+#         # split test row into input and output columns
+#         testX, testy = test[i, :-1], test[i, -1]
+#         # fit model on history and make a prediction
+#         yhat = train.random_forest_forecast(history, testX)
+#         # store forecast in list of predictions
+#         predictions.append(yhat)
+#         # add actual observation to history for the next loop
+#         history.append(test[i])
+#         # summarize progress
+#         print('>expected=%.1f, predicted=%.1f' % (testy, yhat))
+#     # estimate prediction error
+#     error = mean_absolute_error(test[:, -1], predictions)
+#     return error, test[:, 1], predictions
